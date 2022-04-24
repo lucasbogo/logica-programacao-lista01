621d31e47ae10eb2271ced386f5292cacb7765ec
Lucas Bogo <76253752+lucasbogo@users.noreply.github.com>
2022-04-24 19:29:03 -0300

    first commiy

---
+++ lista1-7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+float num1, num2, result;
+printf("Tamanho arquivo em bits: ");
+scanf("%f", &num1);
+printf("Velocidade conexao em bits/s: ");
+scanf("%f", &num2);
+result= num1/num2;
+printf("Tempo necessario: %.2f segundos",result);
+return 0;
+}
