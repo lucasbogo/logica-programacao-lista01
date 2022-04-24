c36efc7c2457827df5b1c29c89a63806cf9d8403
Lucas Bogo <76253752+lucasbogo@users.noreply.github.com>
2022-04-24 20:19:02 -0300

    Update lista1-5.c

--- lista1-5.c
+++ lista1-5.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-int main() {
-int num1, num2, result;
-printf("Kilometros percorridos: ");
-scanf("%d", &num1);
-printf("Litros de combustivel gasto: ");
-scanf("%d", &num2);
-result= num1/num2;
-printf("%d Km/L", result);
+int main() 
+{
+  int num1, num2, result;
+  
+    printf("Kilometros percorridos: ");
+    scanf("%d", &num1);
+    printf("Litros de combustivel gasto: ");
+    scanf("%d", &num2);
+    result= num1/num2;
+    printf("%d Km/L", result);
+  
 return 0;
 }
