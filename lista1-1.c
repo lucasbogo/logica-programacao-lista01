0f621e58d65b2d841e6f488a2ec066cc1a50829e
Lucas Bogo <76253752+lucasbogo@users.noreply.github.com>
2022-04-24 20:14:13 -0300

    Update lista1-1.c

--- lista1-1.c
+++ lista1-1.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+  
+  int num,result;
 
-int num,result;
-
-printf("informar um numero:");
-scanf("%d",&num);
-result = num + 2;
-printf("%d+2=%d", num,result);
+    printf("informar um numero:");
+    scanf("%d",&num);
+    result = num + 2;
+    printf("%d+2=%d", num,result);
 
 return 0;
 
