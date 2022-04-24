2df9a2be6a038fabc8958c26a9de820dff5da450
Lucas Bogo <76253752+lucasbogo@users.noreply.github.com>
2022-04-24 20:19:53 -0300

    Update lista1-6.c

--- lista1-6.c
+++ lista1-6.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 int main()
 {
-float num1, num2, num3, result;
-printf("Informar o comprimento da sala: ");
-scanf("%f", &num1);
-printf("Informar a largura da sala: ");
-scanf("%f", &num2);
-printf("informar o valor do metro quadrado: ");
-scanf("%f", &num3);
-result= num1*num2*num3;
-printf("Custo total: R$ %.2f",result);
+  float num1, num2, num3, result;
+  
+    printf("Informar o comprimento da sala: ");
+    scanf("%f", &num1);
+    printf("Informar a largura da sala: ");
+    scanf("%f", &num2);
+    printf("informar o valor do metro quadrado: ");
+    scanf("%f", &num3);
+    result= num1*num2*num3;
+    printf("Custo total: R$ %.2f",result);
+  
 return 0;
 }
