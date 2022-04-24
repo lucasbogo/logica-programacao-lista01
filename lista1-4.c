c6468677a4dc50f9b8761b2d015257e277d78127
Lucas Bogo <76253752+lucasbogo@users.noreply.github.com>
2022-04-24 20:17:34 -0300

    Update lista1-4.c

--- lista1-4.c
+++ lista1-4.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
 
-float num1, num2, real;
+  float num1, num2, real;
 
-printf("Valor em dolar: ");
-scanf("%f", &num1);
+    printf("Valor em dolar: ");
+    scanf("%f", &num1);
 
-printf("Cotacao dia: ");
-scanf("%f", &num2);
+    printf("Cotacao dia: ");
+    scanf("%f", &num2);
 
-real = num1 * num2;
+    real = num1 * num2;
 
-printf("Valor R$ %.2f",real);
+    printf("Valor R$ %.2f",real);
 
 return 0;
 
