b49ae3c8fb8aa7a44dfb3f7effbe2efbe64463a3
Lucas Bogo <76253752+lucasbogo@users.noreply.github.com>
2022-04-24 18:58:51 -0300

    first commit

---
+++ lista1-4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+float num1, num2, real;
+
+printf("Valor em dolar: ");
+scanf("%f", &num1);
+
+printf("Cotacao dia: ");
+scanf("%f", &num2);
+
+real = num1 * num2;
+
+printf("Valor R$ %.2f",real);
+
+return 0;
+
+}
