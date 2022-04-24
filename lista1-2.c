9c190725c6e9a2352ec14995001808dcb6a174d3
Lucas Bogo <76253752+lucasbogo@users.noreply.github.com>
2022-04-24 20:15:09 -0300

    Update lista1-2.c

--- lista1-2.c
+++ lista1-2.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
 
-int num1, num2, soma, sub, mult, div;
+  int num1, num2, soma, sub, mult, div;
 
-printf("informar primeiro valor:");
-scanf("%d", &num1);
+    printf("informar primeiro valor:");
+    scanf("%d", &num1);
 
-printf("informar segundo valor:");
-scanf("%d", &num2);
+    printf("informar segundo valor:");
+    scanf("%d", &num2);
 
-soma= num1+num2;
-sub= num1-num2;
-mult= num1*num2;
-div= num1*num2;
+    soma= num1+num2;
+    sub= num1-num2;
+    mult= num1*num2;
+    div= num1*num2;
 
-printf("Soma= %d / Sub= %d / Mult= %d / Div= %d",soma, sub, mult, div);
+    printf("Soma= %d / Sub= %d / Mult= %d / Div= %d",soma, sub, mult, div);
 
 return 0;
 
